a058bea3a801d42f478624b35c3feba2152fd108
vivaan120 <83453732+vivaan120@users.noreply.github.com>
2021-06-19 17:56:17 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+var name_of_the_student_array= [];
+function submit(){
+    var name_1= document.getElementById("Name_of_the_student1").value;
+    var name_2= document.getElementById("Name_of_the_student2").value;
+    var name_3= document.getElementById("Name_of_the_student3").value;
+    var name_4= document.getElementById("Name_of_the_student4").value;
+    name_of_the_student_array.push(name_1);
+    name_of_the_student_array.push(name_2);
+    name_of_the_student_array.push(name_3);
+    name_of_the_student_array.push(name_4);
+    console.log(name_of_the_student_array);
+    document.getElementById("display_name").innerHTML=name_of_the_student_array;
+    document.getElementById("submit_button").style.display="none";
+    document.getElementById("sort_button").style.display="inline-block";
+}
+function sorting(){
+    name_of_the_student_array.sort();
+    console.log(name_of_the_student_array);
+    document.getElementById("display_name").innerHTML=name_of_the_student_array;
+}
